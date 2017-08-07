dbe645a547cc47fb13be584dc6ee7c5312a4fd8e
rachitGulati <rachitgulati26@gmail.com>
2017-08-07 13:00:27 +0530

    added right user

--- gulpfile.js
+++ gulpfile.js
@@ -170,7 +170,7 @@ gulp.task('wiredep', () => {
 
 gulp.task('build', ['lint', 'html', 'images', 'fonts', 'extras'], () => {
   return gulp.src('dist/**/*').pipe($.size({title: 'build', gzip: true}))
-  .pipe(ghPages({ branch: 'master', user: { name: 'rachitGulati', email: 'rahitgulati26@gmail.com'} }));
+  .pipe(ghPages({ branch: 'master', user: { name: 'rachitGulati', email: 'rachitgulati26@gmail.com'} }));
 });
 
 gulp.task('default', () => {
